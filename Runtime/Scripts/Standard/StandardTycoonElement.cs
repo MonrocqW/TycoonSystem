ffc6713a898c2d5282b64a5c93f6d8a25f49d62d
wmonrocq <william.monrocq@ls-vr.com>
2025-02-22 19:42:48 +0100

    Standard elements

---
+++ Runtime/Scripts/Standard/StandardTycoonElement.cs
@@ -0,0 +1,10 @@
+using TycoonSystem.Core;
+using UnityEngine;
+
+namespace TycoonSystem.Standard
+{
+    [CreateAssetMenu(menuName = "Tycoon System/Standard Element", order = 0)]
+    public class StandardTycoonElement : AbstractTycoonElement
+    {
+    }
+}
